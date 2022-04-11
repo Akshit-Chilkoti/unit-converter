12e0cd5d1ecd5da90df83d45d1bdd80f2acd6cb2
Akshit-Chilkoti <akshitchilkoti21@gmail.com>
2022-04-11 11:44:57 +0530

    User Input

---
+++ unit-converter.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+#include <windows.h>
+using namespace std;
+
+int main() {
+    int distanceInput;
+    int distanceOutput;
+    cout << "Welcome to the CLI Unit Converter!\n" << "Enter the distance in Centimeter: \n";
+    cin >> distanceInput;
+    return 0;
+}
