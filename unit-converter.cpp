d4d5b0190130b402e02a9308832d90a1d3f4f592
Akshit-Chilkoti <akshitchilkoti21@gmail.com>
2022-04-11 12:30:41 +0530

    Gives an Output

--- unit-converter.cpp
+++ unit-converter.cpp
@@ -4,9 +4,12 @@
 using namespace std;
 
 int main() {
+    system("color a && cls");
     int distanceInput;
     int distanceOutput;
     cout << "Welcome to the CLI Unit Converter!\n" << "Enter the distance in Centimeter: \n";
     cin >> distanceInput;
+    int distnceOutput = distanceInput / 100;
+    cout << distanceOutput;
     return 0;
 }
